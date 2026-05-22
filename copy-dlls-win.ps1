968a09aad5495814078bb84957a81a6bef37adfc
Anthony Maxwell <anthony.maxwell@nwcc.nsw.edu.au>
2026-05-22 21:27:54 +1000

    Added script to copy dlls in for windows for convenience

---
+++ copy-dlls-win.ps1
@@ -0,0 +1,4 @@
+Copy-Item C:\SDL2\x86_64-w64-mingw32\bin\SDL2.dll .pio\build\desktop\      
+Copy-Item C:\msys64\ucrt64\bin\libgcc_s_seh-1.dll .pio\build\desktop\
+Copy-Item C:\msys64\ucrt64\bin\libwinpthread-1.dll .pio\build\desktop\
+Copy-Item C:\msys64\ucrt64\bin\libstdc++-6.dll .pio\build\desktop\
